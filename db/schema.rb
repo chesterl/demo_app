e9c14acb69a27a3e8d00aaf29e33e5c2e74b89c5
Chester Lam <chesterlam620@gmail.com>
2014-04-01 21:13:26 +0800

    Simplified the Users controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140314133021) do
+ActiveRecord::Schema.define(version: 20140314150407) do
 
   create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
